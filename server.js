0964119d46df55b4515c511a50c5d98b08446d80
Steve Brownlee <chortlehoort@gmail.com>
2019-07-13 10:33:52 -0500

    Added custom routes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('api.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('api.json')
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3000
 
-server.use(middlewares);
-server.use(router);
+server.use(jsonServer.rewriter({
+    '/animalia': '/animals?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(middlewares)
+server.use(router)
 
-server.listen(port);
+server.listen(port, () => {
+    console.log('JSON Server is running')
+})
