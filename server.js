cdd48fc600db1c7a5db4202d5fc9dc491b7fb646
Steve Brownlee <chortlehoort@gmail.com>
2021-09-05 20:19:07 +0000

    New JSON

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('api.json')
+const router = jsonServer.router('database.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000
 
-server.use(jsonServer.rewriter({
-    '/animalia': '/animals?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location'
-}))
+#server.use(jsonServer.rewriter({
+#    '/animalia': '/animals?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location'
+#}))
 server.use(middlewares)
 server.use(router)
 
